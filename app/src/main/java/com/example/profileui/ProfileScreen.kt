e2949587588b58d3a66991a522eb13c9180105fe
mohandfares <farestecweb@gmail.com>
2021-08-06 20:36:44 +0100

    Profile Description

--- app/src/main/java/com/example/profileui/ProfileScreen.kt
+++ app/src/main/java/com/example/profileui/ProfileScreen.kt
@@ -4,13 +4,11 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.MoreVert
-import androidx.compose.material.icons.filled.NotificationImportant
-import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.filled.*
 import androidx.compose.material.icons.twotone.Notifications
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -18,10 +16,14 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.profileui.ui.theme.ProfileUITheme
@@ -33,6 +35,11 @@ fun ProfileScreen() {
         TopBar(name = "faresmohand_official     ",modifier = Modifier.padding(10.dp))
         Spacer(modifier = Modifier.height(4.dp))
         ProfileSection()
+        Spacer(modifier = Modifier.height(10.dp))
+        ButtonSection(modifier = Modifier
+            .fillMaxWidth()
+            .padding(20.dp))
+        Spacer(modifier = Modifier.height(10.dp))
     }
 }
 
@@ -94,6 +101,17 @@ fun ProfileSection(
             Spacer(modifier = Modifier.width(16.dp))
             StatSection(modifier = Modifier.weight(10f))
         }
+        ProfileDescription(
+            displayName = "فارس محند",
+            description = "#T ❤\n" +
+                    "\uD83D\uDCBB☕\n" +
+                    "software engineer\n" +
+                    "Programmer, Mobile Developer (Java Kotlin Android)\n" +
+                    "M'y life -> programming, books, sport, music (RAP)",
+            url = "https://www.facebook.com/fares.mohand.9/",
+            followedBy = listOf("tourisme_const","Sofiane Merad"),
+            otherCount = 213
+        )
     }
 }
 
@@ -149,5 +167,141 @@ fun ProfileStat(
         )
         Spacer(modifier = Modifier.height(4.dp))
         Text(text = text)
+    }
+}
+
+@Composable
+fun ProfileDescription(
+    displayName: String,
+    description: String,
+    url: String,
+    followedBy: List<String>,
+    otherCount: Int
+) {
+    val letterSpacing = 0.5.sp
+    val lineHeight = 20.sp
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 20.dp)
+    ) {
+        Text(
+            text = displayName,
+            fontWeight = FontWeight.Bold,
+            letterSpacing = letterSpacing,
+            lineHeight = lineHeight
+        )
+        Text(
+            text = buildAnnotatedString {
+                val hashTagStyle = SpanStyle(
+                    color = Color(0xFF3B5998),
+                    fontWeight = FontWeight.Bold
+                )
+                val descriptionArray = description.split(" ")
+                descriptionArray.forEach { s ->
+                    if (s.startsWith("#")) {
+                        pushStyle(hashTagStyle)
+                        append(s)
+                        pop()
+                    } else {
+                        append(" $s")
+                    }
+                }
+            },
+            letterSpacing = letterSpacing,
+            lineHeight = lineHeight,
+            fontSize = 13.sp
+        )
+        Text(
+            text = url,
+            color = Color(0xFF3B5998),
+            letterSpacing = letterSpacing,
+            lineHeight = lineHeight
+        )
+        if (followedBy.isNotEmpty()) {
+            Text(
+                text = buildAnnotatedString {
+                    val boldStyle = SpanStyle(
+                        color = Color.Black,
+                        fontWeight = FontWeight.Bold
+                    )
+                    append("suivi par ")
+                    followedBy.forEachIndexed { index, name ->
+                        pushStyle(boldStyle)
+                        append(name)
+                        pop()
+                        if (index < followedBy.size - 1) {
+                            append(", ")
+                        }
+                    }
+                    if (otherCount > 2) {
+                        append(" et ")
+                        pushStyle(boldStyle)
+                        append("$otherCount autres")
+                    }
+                },
+                letterSpacing = letterSpacing,
+                lineHeight = lineHeight
+            )
+        }
+    }
+}
+
+@Composable
+fun ButtonSection(
+    modifier: Modifier = Modifier
+) {
+    val minWidth = 300.dp
+    val height = 30.dp
+    Row(
+        horizontalArrangement = Arrangement.Start,
+        modifier = modifier
+    ) {
+        ActionButton(
+            text = "Modifier profil",
+            modifier = Modifier
+                .defaultMinSize(minWidth)
+                .height(height)
+        )
+        Spacer(modifier = Modifier.width(10.dp))
+        ActionButton(
+            imageVector = Icons.Default.KeyboardArrowDown,
+            modifier = Modifier.size(height)
+        )
+    }
+}
+
+@Composable
+fun ActionButton(
+    modifier: Modifier = Modifier,
+    text: String? = null,
+    imageVector: ImageVector? = null
+) {
+    Row(
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
+            .border(
+                width = 1.dp,
+                color = Color.LightGray,
+                shape = RoundedCornerShape(5.dp)
+            )
+            .padding(6.dp)
+    ) {
+        if (text != null) {
+            Text(
+                text = text,
+                fontWeight = FontWeight.SemiBold,
+                fontSize = 14.sp
+            )
+        }
+        
+        if (imageVector != null) {
+            Icon(
+                imageVector = imageVector,
+                contentDescription = null,
+                tint = Color.Black
+            )
+        }
     }
 }
